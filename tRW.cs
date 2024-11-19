7e5f8ac344edacdb39d66e1013a1eb462b9a9f03
HaHAjax <128398800+HaHAjax@users.noreply.github.com>
2024-11-18 17:34:12 -0700

    fixed & cleaned pearls

--- tRW.cs
+++ tRW.cs
@@ -14,25 +14,10 @@ namespace tRW
 	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class tRW : Mod
 	{
-        //private static Asset<Texture2D> platinumPearlItem;
-        //private static Asset<Texture2D> platinumPearl;
-        //private static Asset<Texture2D> platinumPearlTile;
-        //private static Asset<Texture2D> goldPearlItem;
-        //private static Asset<Texture2D> goldPearl;
-        //private static Asset<Texture2D> goldPearlTile;
-        //private static Asset<Texture2D> silverPearlItem;
-        //private static Asset<Texture2D> silverPearl;
-        //private static Asset<Texture2D> silverPearlTile;
-        //private static Asset<Texture2D> copperPearlItem;
-        //private static Asset<Texture2D> copperPearl;
-        //private static Asset<Texture2D> copperPearlTile;
         private string coinDir = "tRW/Assets/Misc/Currency/";
 
         public override void Load()
         {
-            //TextureAssets.Item[ItemID.PlatinumCoin] = ModContent.Request<Texture2D>("tRW/Assets/Misc/Currency/PearlPlatinum");
-            //TextureAssets.Item[ItemID.GoldCoin] = ModContent.Request<Texture2D>("tRW/Assets/Misc/Currency/PearlGold");
-
             TextureAssets.Tile[TileID.CopperCoinPile] = ModContent.Request<Texture2D>(coinDir + "CopperPearlTile");
             TextureAssets.Tile[TileID.SilverCoinPile] = ModContent.Request<Texture2D>(coinDir + "SilverPearlTile");
             TextureAssets.Tile[TileID.GoldCoinPile] = ModContent.Request<Texture2D>(coinDir + "GoldPearlTile");
@@ -53,13 +38,10 @@ namespace tRW
 
         public override void Unload()
         {
-            //TextureAssets.Item[ItemID.PlatinumCoin] = ModContent.Request<Texture2D>("tRW/Assets/Misc/Currency/PearlPlatinum");
-            //TextureAssets.Item[ItemID.GoldCoin] = ModContent.Request<Texture2D>("tRW/Assets/Misc/Currency/PearlGold");
-
-            TextureAssets.Tile[TileID.CopperCoinPile] = ModContent.Request<Texture2D>($"Terraria/Images/Tile_{TileID.CopperCoinPile}");
-            TextureAssets.Tile[TileID.SilverCoinPile] = ModContent.Request<Texture2D>($"Terraria/Images/Tile_{TileID.SilverCoinPile}");
-            TextureAssets.Tile[TileID.GoldCoinPile] = ModContent.Request<Texture2D>($"Terraria/Images/Tile_{TileID.GoldCoinPile}");
-            TextureAssets.Tile[TileID.PlatinumCoinPile] = ModContent.Request<Texture2D>($"Terraria/Images/Tile_{TileID.PlatinumCoinPile}");
+            TextureAssets.Tile[TileID.CopperCoinPile] = ModContent.Request<Texture2D>($"Terraria/Images/Tiles_{TileID.CopperCoinPile}");
+            TextureAssets.Tile[TileID.SilverCoinPile] = ModContent.Request<Texture2D>($"Terraria/Images/Tiles_{TileID.SilverCoinPile}");
+            TextureAssets.Tile[TileID.GoldCoinPile] = ModContent.Request<Texture2D>($"Terraria/Images/Tiles_{TileID.GoldCoinPile}");
+            TextureAssets.Tile[TileID.PlatinumCoinPile] = ModContent.Request<Texture2D>($"Terraria/Images/Tiles_{TileID.PlatinumCoinPile}");
 
             TextureAssets.Item[ItemID.CopperCoin] = ModContent.Request<Texture2D>($"Terraria/Images/Item_{ItemID.CopperCoin}");
             TextureAssets.Item[ItemID.SilverCoin] = ModContent.Request<Texture2D>($"Terraria/Images/Item_{ItemID.SilverCoin}");
