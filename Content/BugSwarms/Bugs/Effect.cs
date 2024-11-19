fcb433701451701d314ece6816ac21516cb1c7dc
HaHAjax <128398800+HaHAjax@users.noreply.github.com>
2024-11-18 17:34:32 -0700

    started to add a cool effect

---
+++ Content/BugSwarms/Bugs/Effect.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+
+namespace tRW.Content.BugSwarms.Bugs
+{
+    public class CoolEffect
+    {
+        public float heading;
+        public int speed;
+        public Vector2 flyPosition;
+        public Vector2 prevPosition;
+        public Vector2 velocity;
+        public Vector2 direction;
+
+        Vector2 influence;
+
+        public int awayTimer;
+
+        public int oldPosLength = 7;
+        public Vector2[] oldPositions = new Vector2[7];
+
+        public void SetInitials(Vector2 position)
+        {
+            speed = Main.rand.Next(3, 8);
+            heading += Main.rand.NextFloat(MathHelper.ToRadians(-20), MathHelper.ToRadians(20));
+            direction = heading.ToRotationVector2();
+            velocity = direction * speed;
+            Vector2 initPosVariation = Main.rand.NextVector2Circular(64, 16);
+            flyPosition = position + initPosVariation;
+        }
+
+        public void Update(bool collisionCheck, int timer, Vector2 swarmPosition)
+        {
+            float influenceDistance = flyPosition.Distance(swarmPosition) / 64;
+            Vector2 influenceDirectionTo = flyPosition.DirectionTo(swarmPosition);
+            influence = (influenceDirectionTo) * (influenceDistance);
+            //influence = Vector2.Clamp(influence, new Vector2(-8, -8), new Vector2(8, 8));
+            //if (flyPosition.Distance(swarmPosition) > 256)
+            //{
+
+            //}
+
+            if (collisionCheck)
+            {
+                heading += MathHelper.ToRadians(20);
+                direction = heading.ToRotationVector2();
+                velocity = (influence + direction) * speed;
+            }
+
+
+            if (timer % 4 == 0)
+            {
+                heading += Main.rand.NextFloat(MathHelper.ToRadians(-60), MathHelper.ToRadians(60));
+                direction = heading.ToRotationVector2();
+                velocity = (influence + direction) * speed;
+            }
+
+            if (timer % 15 == 0)
+            {
+                speed = Main.rand.Next(3, 7);
+                velocity = (influence + direction) * speed;
+            }
+
+            if (timer % 360 == 0)
+            {
+                
+            }
+
+            //if (flyPosition.DistanceSQ(swarmPosition) > (256 * 256))
+            //{
+            //    //direction = flyPosition.DirectionTo(swarmPosition);
+
+            //    awayTimer++;
+
+            //    //heading += MathHelper.ToRadians(5);
+            //    //direction = heading.ToRotationVector2();
+            //    //velocity = direction * speed;
+
+
+            //    if (awayTimer >= 15)
+            //    {
+            //        direction = flyPosition.DirectionTo(swarmPosition);
+            //        velocity = (influence + direction) * speed;
+            //    }
+            //    else
+            //    {
+            //        heading += MathHelper.ToRadians(5);
+            //        direction = heading.ToRotationVector2();
+            //        velocity = (influence + direction) * speed;
+            //    }
+            //}
+            //else
+            //{
+            //    awayTimer = 0;
+            //}
+
+            //for (int i = 0; i < oldPositions.Length; i++) oldPositions = new Vector2[5];
+
+
+        }
+
+        public void UpdatePosition()
+        {
+            flyPosition += velocity;
+        }
+
+        public void UpdateOldPositions()
+        {
+            for (int i = oldPositions.Length - 1; i > 0; i--)
+            {
+                oldPositions[i] = oldPositions[i - 1];
+            }
+            oldPositions[0] = flyPosition;
+        }
+    }
+}
---
+++ Content/BugSwarms/Swarms/CoolEffectSwarm.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
+using tRW.Content.BugSwarms.Bugs;
+
+namespace tRW.Content.BugSwarms.Swarms;
+
+public class CoolEffectSwarm : ModNPC
+{
+    public override string Texture => "tRW/Assets/NPCs/FlySwarm";
+
+    private int maxFlies = 50;
+    private CoolEffect[] flies = new CoolEffect[50];
+
+    public override void SetDefaults()
+    {
+        NPC.noGravity = true;
+        NPC.chaseable = false;
+        NPC.npcSlots = 5;
+        NPC.damage = 0;
+        NPC.lifeMax = 5;
+        NPC.dontTakeDamage = true;
+        NPC.knockBackResist = 0;
+        NPC.Hitbox = new Rectangle(0, 0, 16, 16);
+
+        NPC.Opacity = 0;
+
+        //base.SetDefaults();
+    }
+
+    public override float SpawnChance(NPCSpawnInfo spawnInfo)
+    {
+        if (Main.dayTime && spawnInfo.Player.ZoneForest)
+        {
+            return SpawnCondition.Overworld.Chance * 0.1f;
+        }
+        else
+        {
+            return 0f;
+        }
+
+
+        //return base.SpawnChance(spawnInfo);
+    }
+
+    public override void OnSpawn(IEntitySource source)
+    {
+        NPC.position.Y += 32; // Spawns two blocks above the ground
+    }
+
+    public int Timer
+    {
+        get => (int)NPC.ai[1];
+        set => NPC.ai[1] = value;
+    }
+
+    public override void AI()
+    {
+        Timer++;
+
+        //Vector2 fliesPosition; // For debugging
+        //Vector2 fliesVelocity; // For debugging
+        //Vector2 fliesHeadingToV2; // For debugging
+
+        for (int i = 0; i < maxFlies; i++)
+        {
+            //fliesVelocity[i] = flies[i].velocity;
+
+            if (flies[i] == null)
+            {
+                flies[i] = new CoolEffect();
+            }
+
+            if (flies[i].velocity == Vector2.Zero)
+            {
+                flies[i].SetInitials(NPC.position);
+
+                //fliesPosition = flies[i].flyPosition;
+                //fliesVelocity[i] = flies[i].velocity;
+            }
+
+            flies[i].Update(!Collision.CanHitLine(flies[i].flyPosition, 1, 1, flies[i].direction * 4 * 16 + flies[i].flyPosition, 32, 32), Timer, NPC.position);
+            flies[i].UpdatePosition();
+            flies[i].UpdateOldPositions();
+            //fliesPosition = flies[i].flyPosition;
+            //fliesHeadingToV2 = flies[i].heading.ToRotationVector2();
+            //fliesVelocity = flies[i].velocity;
+        }
+
+        //base.AI();
+    }
+
+    public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+    {
+        for (int i = 0; i < maxFlies; i++)
+        {
+            if (flies[i] == null)
+            {
+                flies[i] = new CoolEffect();
+            }
+            else
+            {
+                // Update the drawing for the flies
+                Texture2D baseTexture = ModContent.Request<Texture2D>("tRW/Assets/NPCs/CoolEffect", AssetRequestMode.ImmediateLoad).Value;
+
+                Vector2 drawOrigin = baseTexture.Size() / 2;
+                for (int k = 0; k < flies[i].oldPositions.Length; k++)
+                {
+                    // Shoutout to jioumu (IronTristonia) on the tModLoader Discord for making the flies look much smoother!!!!
+                    Vector2 drawPos = flies[i].oldPositions[k] - Main.screenPosition + drawOrigin + new Vector2(0f, NPC.gfxOffY);
+                    //Color color = new(94, 160, 255, 125);
+                    Color color = (Color)GetAlpha(drawColor) * ((flies[i].oldPositions.Length - k) / (float)flies[i].oldPositions.Length); // fix this later
+                    Vector2 scale = new(((flies[i].flyPosition - flies[i].oldPositions[k]).Length() / baseTexture.Width) / 1.1f, 0.225f);
+                    float rotation = (flies[i].flyPosition - flies[i].oldPositions[k]).ToRotation();
+                    Main.EntitySpriteDraw(baseTexture, drawPos, null, color, rotation, drawOrigin, scale, SpriteEffects.None, 0);
+                }
+            }
+
+        }
+
+        return true;
+        //return base.PreDraw(spriteBatch, screenPos, drawColor);
+    }
+
+    public override Color? GetAlpha(Color drawColor) // this needs to return something (look in ExampleBullet)
+    {
+
+
+        return base.GetAlpha(drawColor);
+    }
+}
